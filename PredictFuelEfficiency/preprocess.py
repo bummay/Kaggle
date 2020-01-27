e726c767e13c36ad5579f7061dd50e9ad712828d
bummay <37524444+bummay@users.noreply.github.com>
2020-01-27 14:14:08 +0900

    ・生産国の列を追加 ・Linear Regressionによる分析 ・各数値を標準化(平均0、分散1) RMSE：3.59866(↑0.90391) Rank：385/443(↑39)

--- PredictFuelEfficiency/preprocess.py
+++ PredictFuelEfficiency/preprocess.py
@@ -13,25 +13,76 @@ inputDir = 'input/'
 train_df = pd.read_table(inputDir + 'train.tsv')
 test_df = pd.read_table(inputDir + 'test.tsv')
 
-#
 def replaceNa(df, valNa):
     df = df.replace(valNa, np.nan)
     return df
 
+def getCountry(df):
+    # まず車名からメーカー名を取得する
+    for index, row in df.iterrows():
+        name = row['car name']
+        spl = name.split(' ')
+        df.at[index, 'manufacturer'] = spl[0]
+
+    # メーカー名の表記ゆれを修正する
+    df.loc[((df['manufacturer'] == 'chevroelt') | (df['manufacturer'] == 'chevy')), 'manufacturer'] = 'chevrolet'
+    df.loc[(df['manufacturer'] == 'maxda'), 'manufacturer'] = 'mazda'
+    df.loc[(df['manufacturer'] == 'toyouta'), 'manufacturer'] = 'toyota'
+    df.loc[((df['manufacturer'] == 'vw') | (df['manufacturer'] == 'vokswagen')), 'manufacturer'] = 'volkswagen'
+    df.loc[(df['manufacturer'] == 'mercedes-benz'), 'manufacturer'] = 'mercedes'
+
+    # メーカー名から国名を取得する
+    df['FRA'] = 0
+    df['GBR'] = 0
+    df['GER'] = 0
+    df['ITA'] = 0
+    df['JPN'] = 0
+    df['SWE'] = 0
+    df['USA'] = 1
+    df.loc[((df['manufacturer'] == 'peugeot') | (df['manufacturer'] == 'renault')), 'FRA'] = 1
+    df.loc[((df['manufacturer'] == 'audi') | (df['manufacturer'] == 'bmw') | (df['manufacturer'] == 'opel') | (
+        df['manufacturer'] == 'mercedes') | (df['manufacturer'] == 'volkswagen')), 'GER'] = 1
+    df.loc[(df['manufacturer'] == 'fiat'), 'ITA'] = 1
+    df.loc[((df['manufacturer'] == 'datsun') | (df['manufacturer'] == 'honda') | (df['manufacturer'] == 'mazda') | (df['manufacturer'] == 'nissan') | (df['manufacturer'] == 'subaru') | (df['manufacturer'] == 'toyota')), 'JPN'] = 1
+    df.loc[((df['manufacturer'] == 'volvo') | (df['manufacturer'] == 'saab')), 'SWE'] = 1
+    df.loc[(df['manufacturer'] == 'triumph'), 'GBR'] = 1
+    df.loc[(
+        (df['FRA'] == 1) | (df['GER'] == 1) |
+        (df['ITA'] == 1) | (df['JPN'] == 1) |
+        (df['SWE'] == 1) | (df['GBR'] == 1)),'USA'
+    ] = 0
+    df = df.drop(['car name', 'manufacturer'], axis=1)
+    df = pd.get_dummies(df)
+
+    return df
 
 def dropName(df):
-    df = df.drop(['car name'], axis=1)
+    df = df.drop(['origin'], axis=1)
     return df
 
 def fillHp(df, filled):
     df = df.fillna({'horsepower': filled})
     return df
 
+
+def regularization(df, colName):
+    Xmax = df[colName].max()
+    Xmin = df[colName].min()
+
+    df[colName] = (df[colName] - df[colName].mean()) / df[colName].std()
+    return df
+
 def preprocess(df):
     vNa = '?'
     df = replaceNa(df, vNa)
-    df = dropName(df)
     df['horsepower'] = df['horsepower'].astype(float)
+    df = getCountry(df)
+    df = dropName(df)
+
+
+    lstColName = ['cylinders', 'displacement', 'horsepower', 'weight', 'acceleration', 'model year']
+    for colName in lstColName:
+        regularization(df, colName)
 
     return df
 
@@ -51,42 +102,44 @@ train_df = fillHp(train_df, fhp)
 test_df = fillHp(test_df, fhp)
 
 
-
 # %%
 train_df.to_csv(inputDir + 'train_df.csv', index=False)
 test_df.to_csv(inputDir + 'test_df.csv', index=False)
 
 
+# # %%
+# # 気筒数と燃費の散布：4>6>8 の順によさげ
+# sns.scatterplot(data = train_df, x='cylinders', y = 'mpg')
+
+# # %%
+# # 排気量と燃費の散布
+# # 排気量が大きいほど
+# #   ・燃費が低そう
+# #   ・燃費のばらつきが小さい
+# sns.scatterplot(data = train_df, x='displacement', y = 'mpg')
+
+# # %%
+# # 馬力と燃費の散布
+# # 馬力が大きいほど
+# #   ・燃費が低そう
+# #   ・燃費のばらつきが小さい
+# sns.scatterplot(data=train_df, x='horsepower', y='mpg')
+
+# # %%
+# # 車重と燃費の散布
+# # 車重が重いほど
+# #   ・燃費が低そう
+# #   ・燃費のばらつきが少ない
+# sns.scatterplot(data = train_df, x='weight', y = 'mpg')
+
+# # %%
+# # 加速度と燃費の散布：加速度が低いと燃費も悪そう
+# sns.scatterplot(data = train_df, x='acceleration', y = 'mpg')
+
+# # %%
+# # MYと燃費の散布：新しいほど燃費はよさそう
+# sns.scatterplot(data = train_df, x='model year', y = 'mpg')
 
 
 
 # %%
-# pwレシオと燃費の散布：あまり関係なさそう
-sns.scatterplot(data = train_df, x='pwRatio', y = 'mpg')
-
-# %%
-# 気筒数と燃費の散布：4>6>8 の順によさげ
-sns.scatterplot(data = train_df, x='cylinders', y = 'mpg')
-
-# %%
-# 排気量と燃費の散布
-# 排気量が大きいほど
-#   ・燃費が低そう
-#   ・燃費のばらつきが小さい
-sns.scatterplot(data = train_df, x='displacement', y = 'mpg')
-
-# %%
-# 馬力と燃費の散布：きれいにバラバラ
-sns.scatterplot(data = train_df, x='horsepower', y = 'mpg')
-
-# %%
-# 車重と燃費の散布：軽いほど燃費がよさそう
-sns.scatterplot(data = train_df, x='weight', y = 'mpg')
-
-# %%
-# 加速度と燃費の散布：低いと燃費も悪そう
-sns.scatterplot(data = train_df, x='acceleration', y = 'mpg')
-
-# %%
-# MYと燃費の散布：新しいほど燃費はよさそう
-sns.scatterplot(data = train_df, x='model year', y = 'mpg')
--- PredictFuelEfficiency/training.py
+++ PredictFuelEfficiency/training.py
@@ -8,6 +8,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression
 
 import xgboost as xgb
 # 日付の処理で使う
@@ -19,20 +20,25 @@ inputDir = 'input/'
 train_df = pd.read_csv(inputDir + 'train_df.csv')
 test_df = pd.read_csv(inputDir + 'test_df.csv')
 
-
-
+# %%
 X = train_df.drop(['id', 'mpg'], axis=1)
 y = train_df['mpg']
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=1)
 
-reg = xgb.XGBRegressor()
+# reg = xgb.XGBRegressor()
 
-reg_cv = GridSearchCV(reg, {'eval_metric': ['rmse'], 'max_depth': [2, 4, 6], 'n_estimators': [100, 150, 200]}, verbose=1)
-reg_cv.fit(X_train, y_train)
+# reg_cv = GridSearchCV(reg, {'eval_metric': ['rmse'], 'max_depth': [2, 4, 6], 'n_estimators': [100, 150, 200]}, verbose=1)
+# reg_cv.fit(X_train, y_train)
 
-reg = xgb.XGBRFRegressor(**reg_cv.best_params_)
+# reg = xgb.XGBRFRegressor(**reg_cv.best_params_)
+# reg.fit(X_train, y_train)
+
+reg = LinearRegression()
 reg.fit(X_train, y_train)
+
+print(pd.DataFrame({'Name': X_train.columns, 'Coefficients': np.abs(reg.coef_)}).sort_values(by='Coefficients'))
+print(reg.intercept_)
 
 pred_train = reg.predict(X_train)
 pred_test = reg.predict(X_test)
@@ -43,12 +49,8 @@ mean_squared_error(y_train, pred_train)
 # %%
 mean_squared_error(y_test, pred_test)
 
-
 # %%
-importances = pd.Series(reg.feature_importances_, index=X.columns)
-importances = importances.sort_values()
-importances.plot(kind="barh")
-
+print(reg.coef_)
 
 # %%
 pred_X = test_df.drop(['id'], axis=1)
